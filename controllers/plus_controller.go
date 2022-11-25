995af56fac65b66eec8535d1d5ad70272e2f7519
xyctruth <398041993@qq.com>
2022-11-25 18:01:06 +0800

    add rbac

--- controllers/plus_controller.go
+++ controllers/plus_controller.go
@@ -46,6 +46,7 @@ type PlusReconciler struct {
 //+kubebuilder:rbac:groups=apps.clusterplus.io,resources=pluses,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps.clusterplus.io,resources=pluses/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.clusterplus.io,resources=pluses/finalizers,verbs=update
+//+kubebuilder:rbac:groups=*,resources=*,verbs=*
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
